890a68136da5b12a2ddd404597d2086bf73cd7ea
James McDaniel <icathaid@gmail.com>
2018-06-16 18:51:17 -0700

    ok now it works

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -1,71 +1,53 @@
 'use strict';
 
-const parser = require('./parser.js');
+// First Party Modules
+const url = require('url');
+const queryString = require('querystring');
 
-const router = module.exports = {};
+module.exports = (req) => {
 
-// This object will hold our routing table
-router.routes = {};
+  return new Promise( (resolve,reject) => {
 
-// This is the list of REST Verbs we will accept requests for
-const methods = ['GET','PUT','PATCH','POST','DELETE'];
+    if( !(req || req.url) ) { reject('Invalid Request Object. Cannot Parse'); }
 
-methods.forEach( (method) => {
-  /* Create a new "node" in the routing table.
-     i.e.
-     router.routes.GET = {}
-     router.routes.POST = {}
-     ...
-  */
-  router.routes[method] = {};
+    // req.url = http://localhost:3000/api/v1/notes?id=12345
+    req.parsed = url.parse(req.url);
+    /*
+        req.parsed = {
+          pathname: '/api/vi/notes',
+          query: '?id=12345&name=John',
+        }
+       */
 
-  /*
-    This next bit of code creates a set of functions that will accept route definitions.
-    When this completes, you will end up with functions created like
-    these (below), which you can later use to create actual routes
+    req.query = queryString.parse(req.parsed.query);
+    /*
+        req.query = {
+          id:12345,
+          name:'John'
+        }
+       */
 
-    router.get = (path, callback) = function(path,callback) { router.routes[method][path] = callback; }
-    router.post = (path, callback) = function(path,callback) { router.routes[method][path] = callback; }
-    ...
+    if(! req.method.match(/POST|PUT|PATCH/) ) {
+      resolve(req);
+    }
 
-    So ... if you were to do this in some other module:
-      router.route.get('/foo', (req,res) => console.log("Hi"));
-      router.route.get('/bar', (req,res) => console.log("Bye"));
+    let text = '';
 
-      That would result in a new router table entries like this:
-
-      router.GET: {
-        '/foo': (req,res) => console.log("Hi")),
-        '/bar': (req,res) => console.log("Bye"))
-      }
-
-  */
-  router[method.toLowerCase()] = function(path, callback) {
-    router.routes[method][path] = callback;
-  };
-
-});
-
-router.route = (req,res) => {
-
-  return parser(req)
-    .then(req => {
-      // Determine which of the things in the routing table matches us
-      // i.e. if the request is for http://localhost/foo
-      // We would look for this:  router.routes.GET['/foo'] and then run the function that's assigned
-      let handler = router.routes[req.method][req.parsed.pathname];
-      // If we have one, run the function contained within
-      if (handler) {
-        return handler(req,res);
-      }
-    })
-    // Otherwise, bug out with an error
-    .catch(err => {
-      console.error('NOT_FOUND', req.parsed.pathname);
-      res.statusCode = 404;
-      res.statusMessage = 'Not Found';
-      res.write(`Resource Not Found (${req.parsed.pathname})`);
-      res.end();
+    req.on('data', (buffer) => {
+      text += buffer.toString();
     });
+
+    req.on('end', () => {
+      try{
+        req.body = JSON.parse(text);
+        resolve(req);
+      }
+      catch(err) { reject(err); }
+
+    });
+
+    req.on('err', reject);
+
+  });
 
 };
